5e1325392bbc9c5f505e08a453bec2388a291b0d
Amir228702 <61585066+Amir228702@users.noreply.github.com>
2020-03-06 13:36:18 +0600

    add: before and after elements in hero section

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,29 @@
-var gulp         = require('gulp');
+const {src,dest,watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
-var browserSync  = require('browser-sync').create();
-
-var sass         = require('gulp-sass');
-
-var autoprefixer = require('gulp-autoprefixer');
-
-var concatCss    = require('gulp-concat-css');
-
-var cleanCSS     = require('gulp-clean-css');
-
-var rename       = require("gulp-rename");
-
-var uglify       = require('gulp-uglify');
-
-gulp.task('serve', ['sass'], function() {
-
-browserSync.init({
-
-server: "src/"
-
-});
-
-watch("./sass/*.sass", ['sass']);
-
-watch("./*.html").on('change', browserSync.reload);
-
-});
-
-function sass() {
-
-return src("src/sass/*.sass")
-
-.pipe(sass().on('error', sass.logError))
-
-.pipe(autoprefixer({
-
-browsers: ['last 2 versions'],
-
-cascade: false
-
-}))
-
-.pipe(concatCss("style.css"))
-
-.pipe(dest("./css"))
-
-.pipe(browserSync.stream());
-
+function bs() {
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*html").on("change", browserSync.reload);
+    watch("./sass/**/*.sass",serveSass);
+    watch("./scss/**/*.scss",serveSass);
+    watch("./js/*.js").on("change", browserSync.reload);
 };
 
-function mincss() {
-
-return src("./css/*.css")
-
-.pipe(rename({suffix: ".min"}))
-
-.pipe(cleanCSS())
-
-.pipe(dest("./css"));
+function serveSass() {
+    return src("./sass/*.sass","./scss/*.scss")
+        .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
 };
 
-task('min',['mincss]']);
+exports.serve = bs;
