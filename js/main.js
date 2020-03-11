5dbf2d1fef75a5655d08fde294e65bc9cfe859e6
Amir228702 <61585066+Amir228702@users.noreply.github.com>
2020-03-11 22:10:34 +0600

    feat: close modal on ESC and onclick

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]')
-    const closeBtn=document.querySelector('.modal__close')
+    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+    const closeBtn=document.querySelector('.modal__close');
+    
     const switchModal = () =>{
         modal.classList.toggle('modal--visible');
     }
     modalBtn.forEach(element =>{
         element.addEventListener('click', switchModal);
     });
-    closeBtn.addEventListener('click',switchModal);
+    closeBtn.addEventListener('click', switchModal);
+    document.addEventListener("click", e=>{
+        if(e.target==modal){
+            modal.classList.remove("modal--visible");
+        }
+    })
+    document.addEventListener("keyup",e =>{
+        const key = e.keyCode ;
+        if(key===27){
+            document.querySelector(".modal").classList.remove("modal--visible");
+        }
+    })
 });
+
