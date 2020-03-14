08108ad6f30555f96a5baa06f66c6c75a607a2be
Amir228702 <61585066+Amir228702@users.noreply.github.com>
2020-03-14 16:24:33 +0600

    feat:add projects

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const switchModal = () =>{
         modal.classList.toggle('modal--visible');
     }
-    /*
+
     modalBtn.forEach(element =>{
         element.addEventListener('click', switchModal);
     });
@@ -27,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     })
 });
 */
+
 $(document).ready(function() {
     var modal = $('.modal'),
         modalBtn = $('[data-toggle=modal]'),
@@ -55,4 +56,21 @@ $(document).ready(function() {
         $("html, body").animate({ scrollTop: 0 }, 800);
         return false;
         });
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+          },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+    })
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets=$('.swiper-pagination');
+
+    next.css('left',prev.width()+10 +bullets.width()+10)
+    bullets.css('left',prev.width()+10)
 });
