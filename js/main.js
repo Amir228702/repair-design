4e3e8432ae852060a28e98d9a288203164e25166
Amir228702 <61585066+Amir228702@users.noreply.github.com>
2020-03-14 20:41:58 +0600

    last try

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeBtn=document.querySelector('.modal__close');
-    
-    const switchModal = () =>{
-        modal.classList.toggle('modal--visible');
-    }
-
-    modalBtn.forEach(element =>{
-        element.addEventListener('click', switchModal);
-    });
-    
-    closeBtn.addEventListener('click', switchModal);
-    
-    document.addEventListener("click", e=>{
-        if(e.target==modal){
-            modal.classList.remove("modal--visible");
-        }
-    })
-    document.addEventListener("keyup",e =>{
-        const key = e.keyCode ;
-        if(key===27){
-            document.querySelector(".modal").classList.remove("modal--visible");
-        }
-    })
-});
 $(document).ready(function() {
     var modal = $('.modal'),
         modalBtn = $('[data-toggle=modal]'),
@@ -71,4 +44,31 @@ $(document).ready(function() {
     next.css('left', prev.width() + 10 + bullets.width() + 10)
     bullets.css('left',prev.width() + 10)
 });
+/*
+document.addEventListener("DOMContentLoaded", function(event) { 
+    const modal = document.querySelector('.modal');
+    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+    const closeBtn=document.querySelector('.modal__close');
+    
+    const switchModal = () =>{
+        modal.classList.toggle('modal--visible');
+    }
 
+    modalBtn.forEach(element =>{
+        element.addEventListener('click', switchModal);
+    });
+    
+    closeBtn.addEventListener('click', switchModal);
+    
+    document.addEventListener("click", e=>{
+        if(e.target==modal){
+            modal.classList.remove("modal--visible");
+        }
+    })
+    document.addEventListener("keyup",e =>{
+        const key = e.keyCode ;
+        if(key===27){
+            document.querySelector(".modal").classList.remove("modal--visible");
+        }
+    })
+});*/
